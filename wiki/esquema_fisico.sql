1089810f3d961eddc7d9da78d771c5d95ac8de1f
João Cardoso <jmmesquitacardoso@gmail.com>
2014-04-02 21:32:16 +0100

    .sql bonito

--- wiki/esquema_fisico.sql
+++ wiki/esquema_fisico.sql
@@ -5,66 +5,23 @@ DROP TABLE IF EXISTS Link CASCADE;
 DROP TABLE IF EXISTS LinkNoticia CASCADE;
 DROP TABLE IF EXISTS NoticiaCategoria CASCADE;
 DROP TABLE IF EXISTS Categoria CASCADE;
+DROP TABLE IF EXISTS Interesse CASCADE;
+DROP TABLE IF EXISTS Editor CASCADE;
+DROP TABLE IF EXISTS Mensagem CASCADE;
+DROP TABLE IF EXISTS Amizade CASCADE;
+DROP TABLE IF EXISTS AvaliarNoticia CASCADE;
+DROP TABLE IF EXISTS AvaliarComentario CASCADE;
 DROP TYPE IF EXISTS tipo CASCADE;
 DROP TYPE IF EXISTS estado CASCADE;
 
 CREATE TYPE tipo AS ENUM ('editor','moderador');
 CREATE TYPE estado AS ENUM ('ban', 'bantemp', 'normal');
 
-/* Notícias */
-CREATE TABLE IF NOT EXISTS Noticia(
-	idNoticia SERIAL PRIMARY KEY,
-	conteudo VARCHAR NOT NULL, 
-	titulo VARCHAR(85) NOT NULL, 
-	data_post  DATE DEFAULT CURRENT_DATE, 
-	idEditor INTEGER REFERENCES Editor
-	);
-
-/* Comentários */
-CREATE TABLE IF NOT EXISTS Comentario(
-	idComentario SERIAL PRIMARY KEY,
-	conteudo VARCHAR(140) NOT NULL,
-	idEditor INTEGER REFERENCES Editor, 
-	idNoticia INTEGER REFERENCES Noticia
-	);
-
-/* Links */
-CREATE TABLE IF NOT EXISTS Link(
-	idLink SERIAL PRIMARY KEY,
-	homeLink VARCHAR NOT NULL,
-	href VARCHAR NOT NULL
-	);
-
-/* Links para Noticias */
-CREATE TABLE IF NOT EXISTS LinkNoticia(
-	idNoticia INTEGER REFERENCES Noticia,
-	idLink INTEGER REFERENCES Link
-	);
-
-/* Categorias */
-CREATE TABLE IF NOT EXISTS Categoria(
-	idCategoria SERIAL PRIMARY KEY,
-	nome VARCHAR NOT NULL
-	);
-
-/* Categorias de Noticias */
-CREATE TABLE IF NOT EXISTS NoticiaCategoria(
-	idNoticia INTEGER REFERENCES Noticia,
-	idCategoria INTEGER REFERENCES Categoria
-	);
-
-/* Interesses */
-CREATE TABLE IF NOT EXISTS Interesse(
-	idEditor INTEGER REFERENCES Editor,
-	idCategoria INTEGER REFERENCES Categoria
-	);
-
 /* Editor */
 CREATE TABLE IF NOT EXISTS Editor(
-	idEditor SERIAL PRIMARY KEY,
+	username VARCHAR(15) PRIMARY KEY NOT NULL,
 	nome VARCHAR NOT NULL,
 	localidade VARCHAR NOT NULL,
-	username VARCHAR(15) UNIQUE NOT NULL,
 	pass VARCHAR NOT NULL,
 	email VARCHAR UNIQUE NOT NULL,
 	profissao VARCHAR NOT NULL,
@@ -75,26 +32,72 @@ CREATE TABLE IF NOT EXISTS Editor(
 	CHECK (char_length(username) > 6)
 	);
 
+/* Notícias */
+CREATE TABLE IF NOT EXISTS Noticia(
+	idNoticia SERIAL PRIMARY KEY,
+	conteudo VARCHAR NOT NULL, 
+	titulo VARCHAR(85) NOT NULL, 
+	data_post  DATE DEFAULT CURRENT_DATE, 
+	username VARCHAR(15) REFERENCES Editor
+	);
+
+/* Comentários */
+CREATE TABLE IF NOT EXISTS Comentario(
+	idComentario SERIAL PRIMARY KEY,
+	conteudo VARCHAR(140) NOT NULL,
+	username VARCHAR(15) REFERENCES Editor, 
+	idNoticia INTEGER REFERENCES Noticia
+	);
+
+/* Links */
+CREATE TABLE IF NOT EXISTS Link(
+	href VARCHAR  PRIMARY KEY NOT NULL,
+	homeLink VARCHAR NOT NULL
+	);
+
+/* Links para Noticias */
+CREATE TABLE IF NOT EXISTS LinkNoticia(
+	idNoticia INTEGER REFERENCES Noticia,
+	href VARCHAR REFERENCES Link
+	);
+
+/* Categorias */
+CREATE TABLE IF NOT EXISTS Categoria(
+	nome VARCHAR PRIMARY KEY NOT NULL
+	);
+
+/* Categorias de Noticias */
+CREATE TABLE IF NOT EXISTS NoticiaCategoria(
+	idNoticia INTEGER REFERENCES Noticia,
+	nome VARCHAR REFERENCES Categoria
+	);
+
+/* Interesses */
+CREATE TABLE IF NOT EXISTS Interesse(
+	username VARCHAR(15) REFERENCES Editor,
+	nome VARCHAR REFERENCES Categoria
+	);
+
 /* Mensagem */
 CREATE TABLE IF NOT EXISTS Mensagem(
 	idMensagem SERIAL PRIMARY KEY,
-	emissor INTEGER REFERENCES Editor,
-	recetor INTEGER REFERENCES Editor,
+	emissor VARCHAR(15) REFERENCES Editor,
+	recetor VARCHAR(15) REFERENCES Editor,
 	titulo VARCHAR NOT NULL,
 	conteudo VARCHAR(140) NOT NULL
 	);
 
 /* Amizade */
 CREATE TABLE IF NOT EXISTS Amizade(
-	amigo1 INTEGER REFERENCES Editor,
-	amigo2 INTEGER REFERENCES Editor,
+	amigo1 VARCHAR(15) REFERENCES Editor,
+	amigo2 VARCHAR(15) REFERENCES Editor,
 	CHECK (amigo1<amigo2)
 	);
 
 /* AvaliarNoticia */
 CREATE TABLE IF NOT EXISTS AvaliarNoticia(
 	chave SERIAL PRIMARY KEY,
-	idEditor INTEGER REFERENCES Editor,
+	username VARCHAR(15) REFERENCES Editor,
 	idNoticia INTEGER REFERENCES Noticia,
 	avaliacao INTEGER NOT NULL,
 	CHECK (avaliacao = -1 OR avaliacao = 1)
@@ -103,79 +106,91 @@ CREATE TABLE IF NOT EXISTS AvaliarNoticia(
 /* AvaliarComentario */
 CREATE TABLE IF NOT EXISTS AvaliarComentario(
 	chave SERIAL PRIMARY KEY,
-	idEditor INTEGER REFERENCES Editor,
+	username VARCHAR(15) REFERENCES Editor,
 	idComentario INTEGER REFERENCES Comentario,
 	avaliacao INTEGER NOT NULL,
 	CHECK (avaliacao = -1 OR avaliacao = 1)
 	);
 
-INSERT INTO Editor VALUES(DEFAULT,'Joao Filetes','Ali acola','filetez','987654321','welele@fe.up.pt','autista','asasasa','editor','bantemp');
-INSERT INTO Editor VALUES(DEFAULT,'Wiwson Rabanadas','Ali alem','rabandaz','987654321','welelf@fe.up.pt','autista','asasasa','editor','bantemp');    
-INSERT INTO Editor VALUES(DEFAULT,'Luis Salmao','Ali no caralho','salmaez','987654321','welela@fe.up.pt','autista','asasasa','editor','bantemp');  
-INSERT INTO Editor VALUES(DEFAULT,'Fabio Franganito','Ali no caralhao te foda','franganitoz','987654321','welile@fe.up.pt','autista','asasasa','editor','bantemp');  
+INSERT INTO Editor VALUES('filetez','Joao Filetes','Ali acola','987654321','welele@fe.up.pt','autista','asasasa','editor','bantemp');
+INSERT INTO Editor VALUES('rabandaz','Wiwson Rabanadas','Ali alem','987654321','welelf@fe.up.pt','autista','asasasa','editor','bantemp');    
+INSERT INTO Editor VALUES('tiburoenz','Luis Tubarao','Ali no caralho','987654321','welela@fe.up.pt','autista','asasasa','editor','bantemp');  
+INSERT INTO Editor VALUES('franganitoz','Fabio Franganito','Ali no caralhao te foda','987654321','welile@fe.up.pt','autista','asasasa','editor','bantemp');  
 
-INSERT INTO Noticia VALUES(DEFAULT,'Pinto da Costa acaba de comer outra gaja','titulo','2000-12-16',1);
-INSERT INTO Noticia VALUES(DEFAULT,'Jorge Jesus volta a ladrar','titulo','2000-12-16',2);
-INSERT INTO Noticia VALUES(DEFAULT,'Jorge Jesus engasga-se a falar','titulo','2000-12-16',3);
-INSERT INTO Noticia VALUES(DEFAULT,'Bruno de Carvalho tem clausula de rescisao de 150 milhoes de euros','titulo','2000-12-16',4);
+INSERT INTO Noticia VALUES(DEFAULT,'Pinto da Costa acaba de comer outra gaja','titulo','2000-12-16','filetez');
+INSERT INTO Noticia VALUES(DEFAULT,'Jorge Jesus volta a ladrar','titulo','2000-12-16','rabandaz');
+INSERT INTO Noticia VALUES(DEFAULT,'Jorge Jesus engasga-se a falar','titulo','2000-12-16','tiburoenz');
+INSERT INTO Noticia VALUES(DEFAULT,'Bruno de Carvalho tem clausula de rescisao de 150 milhoes de euros','titulo','2000-12-16','franganitoz');
 
-INSERT INTO Categoria VALUES(DEFAULT, 'desporto');
-INSERT INTO Categoria VALUES(DEFAULT, 'futebol');
-INSERT INTO Categoria VALUES(DEFAULT, 'cinema');
-INSERT INTO Categoria VALUES(DEFAULT, 'literatura');
-INSERT INTO Categoria VALUES(DEFAULT, 'agricultura');
-INSERT INTO Categoria VALUES(DEFAULT, 'politica');
-INSERT INTO Categoria VALUES(DEFAULT, 'sexo');
-INSERT INTO Categoria VALUES(DEFAULT, 'video-jogos');
-INSERT INTO Categoria VALUES(DEFAULT, 'atualidade');
-INSERT INTO Categoria VALUES(DEFAULT, 'gastronomia'); 
+INSERT INTO Categoria VALUES('desporto');
+INSERT INTO Categoria VALUES('futebol');
+INSERT INTO Categoria VALUES('cinema');
+INSERT INTO Categoria VALUES('literatura');
+INSERT INTO Categoria VALUES('agricultura');
+INSERT INTO Categoria VALUES('politica');
+INSERT INTO Categoria VALUES('sexo');
+INSERT INTO Categoria VALUES('video-jogos');
+INSERT INTO Categoria VALUES('atualidade');
+INSERT INTO Categoria VALUES('gastronomia'); 
 
-INSERT INTO NoticiaCategoria VALUES(1,1);
-INSERT INTO NoticiaCategoria VALUES(2,2);
-INSERT INTO NoticiaCategoria VALUES(3,3);
-INSERT INTO NoticiaCategoria VALUES(4,4);
+INSERT INTO NoticiaCategoria VALUES(1,'desporto');
+INSERT INTO NoticiaCategoria VALUES(2,'futebol');
+INSERT INTO NoticiaCategoria VALUES(3,'cinema');
+INSERT INTO NoticiaCategoria VALUES(4,'literatura');
 
-INSERT INTO AvaliarNoticia VALUES(DEFAULT,1,1,1);
-INSERT INTO AvaliarNoticia VALUES(DEFAULT,1,2,1);
-INSERT INTO AvaliarNoticia VALUES(DEFAULT,2,3,-1);
-INSERT INTO AvaliarNoticia VALUES(DEFAULT,3,1,-1);
-INSERT INTO AvaliarNoticia VALUES(DEFAULT,4,2,-1);
-
-/* Editor,Comentario, Avaliacao*/
-INSERT INTO AvaliarComentario VALUES(DEFAULT,1,1,1);
-INSERT INTO AvaliarComentario VALUES(DEFAULT,1,2,1);
-INSERT INTO AvaliarComentario VALUES(DEFAULT,2,3,-1);
-INSERT INTO AvaliarComentario VALUES(DEFAULT,3,1,-1);
-INSERT INTO AvaliarComentario VALUES(DEFAULT,4,2,-1);
+INSERT INTO AvaliarNoticia VALUES(DEFAULT,'filetez',1,1);
+INSERT INTO AvaliarNoticia VALUES(DEFAULT,'filetez',2,1);
+INSERT INTO AvaliarNoticia VALUES(DEFAULT,'rabandaz',3,-1);
+INSERT INTO AvaliarNoticia VALUES(DEFAULT,'tiburoenz',1,-1);
+INSERT INTO AvaliarNoticia VALUES(DEFAULT,'franganitoz',2,-1);
 
 /*Editor,Noticia*/
-INSERT INTO Comentario VALUES(DEFAULT,'GANDA GAJA OH BELHOTE',1,1);
-INSERT INTO Comentario VALUES(DEFAULT,'TCH NUNCA PENSEI',1,2);
-INSERT INTO Comentario VALUES(DEFAULT,'POAH',3,1);
-INSERT INTO Comentario VALUES(DEFAULT,'WELELELELELE',2,3);
-INSERT INTO Comentario VALUES(DEFAULT,'VOU TE MALABAAAAAAAAAAR',1,4);
-INSERT INTO Comentario VALUES(DEFAULT,'TEMOS QUE MUDAR ESTA MERDA',4,2);
+INSERT INTO Comentario VALUES(DEFAULT,'GANDA GAJA OH BELHOTE','filetez',1);
+INSERT INTO Comentario VALUES(DEFAULT,'TCH NUNCA PENSEI','filetez',2);
+INSERT INTO Comentario VALUES(DEFAULT,'POAH','tiburoenz',1);
+INSERT INTO Comentario VALUES(DEFAULT,'WELELELELELE','rabandaz',3);
+INSERT INTO Comentario VALUES(DEFAULT,'VOU TE MALABAAAAAAAAAAR','filetez',4);
+INSERT INTO Comentario VALUES(DEFAULT,'TEMOS QUE MUDAR ESTA MERDA','franganitoz',2);
 
-INSERT INTO Mensagem VALUES(DEFAULT,1,2,'titulo1','ganda cena oh belhoooooteeeee');
-INSERT INTO Mensagem VALUES(DEFAULT,1,3,'titulo2','ganda cena oh filhoooooteeeee');
-INSERT INTO Mensagem VALUES(DEFAULT,1,4,'titulo3','ganda cena oh velhoooooteeeee');
-INSERT INTO Mensagem VALUES(DEFAULT,2,1,'titulo4','olha, cenas aconteceram');
-INSERT INTO Mensagem VALUES(DEFAULT,3,1,'titulo4','olha, hoje nao vai pintar');
+/* Editor,Comentario, Avaliacao*/
+INSERT INTO AvaliarComentario VALUES(DEFAULT,'filetez',1,1);
+INSERT INTO AvaliarComentario VALUES(DEFAULT,'filetez',2,1);
+INSERT INTO AvaliarComentario VALUES(DEFAULT,'rabandaz',3,-1);
+INSERT INTO AvaliarComentario VALUES(DEFAULT,'tiburoenz',1,-1);
+INSERT INTO AvaliarComentario VALUES(DEFAULT,'franganitoz',2,-1);
 
-INSERT INTO Amizade VALUES(1,2);
-INSERT INTO Amizade VALUES(1,3);
-INSERT INTO Amizade VALUES(1,4);
-INSERT INTO Amizade VALUES(2,3);
+INSERT INTO Mensagem VALUES(DEFAULT,'filetez','rabandaz','titulo1','ganda cena oh belhoooooteeeee');
+INSERT INTO Mensagem VALUES(DEFAULT,'filetez','tiburoenz','titulo2','ganda cena oh filhoooooteeeee');
+INSERT INTO Mensagem VALUES(DEFAULT,'filetez','franganitoz','titulo3','ganda cena oh velhoooooteeeee');
+INSERT INTO Mensagem VALUES(DEFAULT,'rabandaz','filetez','titulo4','olha, cenas aconteceram');
+INSERT INTO Mensagem VALUES(DEFAULT,'tiburoenz','filetez','titulo4','olha, hoje nao vai pintar');
 
-INSERT INTO Link VALUES(DEFAULT,'http://www.abola.pt/ojorgejesusladrou','abola.pt');
-INSERT INTO Link VALUES(DEFAULT,'http://www.abola.pt/ojorgejesusengasgouse','abola.pt');
-INSERT INTO Link VALUES(DEFAULT,'http://www.abola.pt/fonsecapensaquestaem2013','abola.pt');
-INSERT INTO Link VALUES(DEFAULT,'http://www.abola.pt/marcosilvacoiso','abola.pt');
-INSERT INTO Link VALUES(DEFAULT,'http://www.abola.pt/olaeusouox','abola.pt');
-INSERT INTO Link VALUES(DEFAULT,'http://www.abola.pt/olacomoestas','abola.pt');
+INSERT INTO Amizade VALUES('filetez','rabandaz');
+INSERT INTO Amizade VALUES('filetez','tiburoenz');
+INSERT INTO Amizade VALUES('filetez','franganitoz');
+INSERT INTO Amizade VALUES('rabandaz','tiburoenz');
+
+INSERT INTO Link VALUES('http://www.abola.pt/ojorgejesusladrou','abola.pt');
+INSERT INTO Link VALUES('http://www.abola.pt/ojorgejesusengasgouse','abola.pt');
+INSERT INTO Link VALUES('http://www.abola.pt/fonsecapensaquestaem2013','abola.pt');
+INSERT INTO Link VALUES('http://www.abola.pt/marcosilvacoiso','abola.pt');
+INSERT INTO Link VALUES('http://www.abola.pt/olaeusouox','abola.pt');
+INSERT INTO Link VALUES('http://www.abola.pt/olacomoestas','abola.pt');
 
 /* Noticia, Link*/
-INSERT INTO LinkNoticia VALUES(1,1);
-INSERT INTO LinkNoticia VALUES(2,3);
-INSERT INTO LinkNoticia VALUES(4,2);
-INSERT INTO LinkNoticia VALUES(4,3);
+INSERT INTO LinkNoticia VALUES(1,'http://www.abola.pt/ojorgejesusladrou');
+INSERT INTO LinkNoticia VALUES(2,'http://www.abola.pt/fonsecapensaquestaem2013');
+INSERT INTO LinkNoticia VALUES(4,'http://www.abola.pt/ojorgejesusengasgouse');
+INSERT INTO LinkNoticia VALUES(4,'http://www.abola.pt/fonsecapensaquestaem2013');
+
+/* Editor, Categoria */
+INSERT INTO Interesse VALUES('filetez','desporto');
+INSERT INTO Interesse VALUES('filetez','futebol');
+INSERT INTO Interesse VALUES('filetez','cinema');
+INSERT INTO Interesse VALUES('filetez','literatura');
+INSERT INTO Interesse VALUES('rabandaz','desporto');
+INSERT INTO Interesse VALUES('tiburoenz','cinema');
+INSERT INTO Interesse VALUES('franganitoz','atualidade');
+INSERT INTO Interesse VALUES('rabandaz','sexo');
+INSERT INTO Interesse VALUES('tiburoenz','video-jogos');
+INSERT INTO Interesse VALUES('franganitoz','literatura');
