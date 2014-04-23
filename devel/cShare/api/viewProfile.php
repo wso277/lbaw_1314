54c8b33b1b8d40ccaf3ad3b519c6ccf9ed321c95
wso277 <oliveira.wilson93@gmail.com>
2014-04-23 13:54:24 +0100

    Added view Profile

--- devel/cShare/api/viewProfile.php
+++ devel/cShare/api/viewProfile.php
@@ -5,5 +5,11 @@ $user = $_GET['username'];
 if (preg_match("/^[^;:\"]{6,15}$/", $user)) {
     $result = getUserByUsername($user);
 
+    if ($result != false) {
+        echo $result;
+    }
+    else {
+        echo "Failure!";
+    }
 
 }
