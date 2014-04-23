721a798a2eb6239fbd4118c6eabb829390810195
wso277 <oliveira.wilson93@gmail.com>
2014-04-23 11:29:28 +0100

    Added register

---
+++ devel/cShare/api/register.php
@@ -0,0 +1,48 @@
+<?php
+include_once('../config/init.php');
+include_once($BASE_DIR.'database/users.php');
+
+$user = $_POST['username'];
+$name = $_POST['name'];
+$location = $_POST['location'];
+$pass = $_POST['pass'];
+$email = $_POST['email'];
+$job = $_POST['job'];
+
+if (preg_match("/^[^;:\"]{6,15}$/", $user)) {
+    if (preg_match("/^[^;:\"]{8,}$/", $pass)) {
+        if (preg_match("/^[a-zA-Z ]+$/", $name)) {
+            if (preg_match("/^[a-zA-Z ]+$/", $location)) {
+                if (preg_match("/^[a-zA-Z ]+$/", $job)) {
+                    if (preg_match("/^[^@]+@[^@]+.[a-zA-Z]{2,6}$/", $email)) {
+                        $result = createUser($name, $user, $location, $job, $email, $pass);
+
+                        if ($result != false) {
+                            echo "Success!";
+                        }
+                        else {
+                            echo "Failure";
+                        }
+                    }
+                    else {
+                        echo "Invalid Email!";
+                    }
+                }
+                else {
+                    echo "Invalid Location!";
+                }
+            }
+            else {
+                echo "Invalid Location!";
+            }
+        }
+        else {
+            echo "Invalid Name!";
+        }
+    }
+    else {
+        echo "Invalid Password!";
+    }
+} else {
+    echo "Invalid Username!";
+}
