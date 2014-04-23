7723fc36dec7b10e7f7d4004eec95a3ecac37401
Fabio Filipe Jesus Silva <fabio.silva93@hotmail.com>
2014-04-23 14:15:06 +0100

    social

---
+++ devel/cShare/api/view_messages.php
@@ -0,0 +1,29 @@
+<?php
+include_once($BASE_DIR, "database/users.php");
+session_start();
+$me = $_GET["username"];
+
+if(preg_match("/^[^;:\"]{6,15}$/", $me)
+{
+	$sentMessages = getSentMessages($me);
+	$receivedMessages = getReceivedMessages($me);
+	
+	foreach($sentMessages as $str)
+	{
+		if(strlen($str["conteudo"]) > 140) 
+			echo "MESSAGE SIZE > 140";
+		
+	}
+	
+	foreach($receivedMessages as $str)
+	{
+		if(strlen($str["conteudo"]) > 140) 
+			echo "MESSAGE SIZE > 140";
+		
+	}
+	
+	$mergedMessages = array_merge($sentMessages, $receivedMessages);
+	
+	return $mergedMessages;
+}
+?>
