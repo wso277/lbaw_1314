87a1b82996d8702b03b1e8a32e34adb3ed31357b
“wso277” <oliveira.wilson93@gmail.com>
2014-05-30 15:10:39 +0100

    fixed init

--- devel/cShare/config/init.php
+++ devel/cShare/config/init.php
@@ -1,33 +1,34 @@
 <?php
 
-session_set_cookie_params(3600, '/~lbaw1353'); //FIXME
-session_start();
+    session_set_cookie_params(3600, '/~lbaw1353'); //FIXME
+    session_start();
 
-$BASE_DIR = '/opt/lbaw/lbaw1353/public_html/devel/'; //FIXME
-$BASE_URL = '/~lbaw1353/devel/'; //FIXME
+    $BASE_DIR = '/opt/lbaw/lbaw1353/public_html/devel/'; //FIXME
+    $BASE_URL = '/~lbaw1353/devel/'; //FIXME
 
-$conn = new PDO('pgsql:host=vdbm;dbname=lbaw1353', 'lbaw1353', 'cQ240ds1'); //FIXME
-$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn = new PDO('pgsql:host=vdbm;dbname=lbaw1353', 'lbaw1353', 'cQ240ds1'); //FIXME
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$conn->exec('SET SCHEMA \'public\''); //FIXME
+    $conn->exec('SET SCHEMA \'public\''); //FIXME
 
-include_once($BASE_DIR . 'lib/smarty/Smarty.class.php');
+    include_once($BASE_DIR . 'lib/smarty/Smarty.class.php');
 
-$smarty = new Smarty;
-$smarty->template_dir = $BASE_DIR . 'templates/';
-$smarty->compile_dir = $BASE_DIR . 'templates_c/';
-$smarty->assign('BASE_URL', $BASE_URL);
+    $smarty = new Smarty;
+    $smarty->template_dir = $BASE_DIR . 'templates/';
+    $smarty->compile_dir = $BASE_DIR . 'templates_c/';
+    $smarty->assign('BASE_URL', $BASE_URL);
 
-$smarty->assign('ERROR_MESSAGES', $_SESSION['error_messages']);
-$smarty->assign('FIELD_ERRORS', $_SESSION['field_errors']);
-$smarty->assign('SUCCESS_MESSAGES', $_SESSION['success_messages']);
-$smarty->assign('FORM_VALUES', $_SESSION['form_values']);
-$smarty->assign('USERNAME', $_SESSION['username']);
-$smarty->assign('PERMISSION', $_SESSION['tipo']);
+    $smarty->assign('ERROR_MESSAGES', $_SESSION['error_messages']);
+    $smarty->assign('FIELD_ERRORS', $_SESSION['field_errors']);
+    $smarty->assign('SUCCESS_MESSAGES', $_SESSION['success_messages']);
+    $smarty->assign('FORM_VALUES', $_SESSION['form_values']);
+    $smarty->assign('USERNAME', $_SESSION['username']);
+    $smarty->assign('PERMISSION', $_SESSION['tipo']);
 
-unset($_SESSION['success_messages']);
-unset($_SESSION['error_messages']);
-unset($_SESSION['field_errors']);
-unset($_SESSION['form_values']);
-?>
+    unset($_SESSION['success_messages']);
+    unset($_SESSION['error_messages']);
+    unset($_SESSION['field_errors']);
+    unset($_SESSION['form_values']);
+    unset($_SESSION['username']);
+    unset($_SESSION['tipo']);
