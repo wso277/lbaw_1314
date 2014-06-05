8ec53deaa66ac314650c13e1c94134a9cbd04304
João Cardoso <jmmesquitacardoso@gmail.com>
2014-06-05 11:51:58 +0100

    registo incrivel

--- devel/cShare/javascript/register.js
+++ devel/cShare/javascript/register.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("#password, #password_confirmation").change(function() {
         var password = $('#password').val();
         var passConf = $('#password_confirmation').val();
-        if(password == passConf && $('#t_and_c').prop('checked')){
+        if(password != "" && passConf != "" && password == passConf && $('#t_and_c').prop('checked')){
             $("#sub").prop('disabled',false);
         } else {
             $("#sub").prop('disabled', true);
@@ -11,7 +11,7 @@ $(document).ready(function() {
     $("#agree").click(function() {
         var password = $('#password').val();
         var passConf = $('#password_confirmation').val();
-        if(password == passConf && $('#t_and_c').prop('checked')){
+        if(password != "" && passConf != "" && password == passConf && $('#t_and_c').prop('checked')){
             $("#sub").prop('disabled',false);
         } else {
             $("#sub").prop('disabled', true);
