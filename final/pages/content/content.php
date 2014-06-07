13a302820e325538922ab2dcd342c3628eaf3750
João Cardoso <jmmesquitacardoso@gmail.com>
2014-06-07 18:16:04 +0100

    doneeeeeeeeeeeeeeeee

--- final/pages/content/content.php
+++ final/pages/content/content.php
@@ -11,17 +11,11 @@
     $i = 0;
 
     foreach ($results as $result) {
-        var_dump($_GET['id']);
-        var_dump($result['idcomentario']);
-        var_dump($result['username']);
         $temp = getCommentLikes($_GET['id'], $result['idcomentario']);
-        var_dump($temp);
         $comments[$i] = array("id" => $result['idcomentario'], "username" => $result['username'], "photo" => getUserPhoto($result['username']), "content" => $result['conteudo'], "likes" => $temp['sum'], "cont" => $i);
         $comments[$i]['content'] = str_replace("\n", "<br>", $comments[$i]['content']);
         $i++;
     }
-
-    //var_dump($comments);
 
     $smarty->assign('content', $content);
     $smarty->assign('comments', $comments);
